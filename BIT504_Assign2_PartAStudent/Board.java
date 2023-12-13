53ddb58c2f4033d683143167222fc21949ba5c3c
mklem27 <moniqueaklem.mk@gmail.com>
2023-12-13 15:25:57 +1300

    Check if player has 3 & check the diagonal in other direction

--- BIT504_Assign2_PartAStudent/Board.java
+++ BIT504_Assign2_PartAStudent/Board.java
@@ -47,21 +47,20 @@ public class Board {
 		if(cells[playerRow][0].content == thePlayer && cells[playerRow][1].content == thePlayer && cells[playerRow][2].content == thePlayer )
 			return true; 
 		
-		 // TODO: Check if the player has 3 in the playerCol.
-		 // Hint: Use the row code above as a starting point, remember that it goes cells[row][column] 
-		
-		
+		 // Check if the player has 3 in the playerCol.
+		if(cells[0][playerCol].content == thePlayer && cells[1][playerCol].content == thePlayer && cells[2][playerCol].content == thePlayer)
+			return true;
 		
 		 // 3-in-the-diagonal
 		if( cells[0][0].content == thePlayer && cells[1][1].content == thePlayer && cells[2][2].content == thePlayer)
 			return true;
 		 
 		
-		// TODO: Check the diagonal in the other direction
+		// Check the diagonal in the other direction
+		if(cells[0][2].content == thePlayer && cells[1][1].content == thePlayer && cells [2][0].content == thePlayer)
+			return true;		
 		
-
-		
-		//no winner, keep playing
+		// No winner, keep playing
 		return false;
 	}
 	
