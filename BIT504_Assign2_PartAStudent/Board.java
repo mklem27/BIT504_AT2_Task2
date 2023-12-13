e49963b844341ca9a2b6f3b81ac4369de43141f4
mklem27 <moniqueaklem.mk@gmail.com>
2023-12-13 15:02:52 +1300

    Checks whether the game ended in a draw

--- BIT504_Assign2_PartAStudent/Board.java
+++ BIT504_Assign2_PartAStudent/Board.java
@@ -27,13 +27,18 @@ public class Board {
 	 /** Return true if it is a draw (i.e., no more EMPTY cells) */ 
 	public boolean isDraw() {
 		 
-		// TODO: Check whether the game has ended in a draw. 
-		// Hint: Use a nested loop (see the constructor for an example). Check whether any of the cells content in the board grid are Player.Empty. If they are, it is not a draw.
-		// Hint: Return false if it is not a draw, return true if there are no empty positions left
-		   
+		// Check whether the game has ended in a draw
+		// Checking cells array for Player.Empty
+		for(int row = 0; row < GameMain.ROWS; ++row) {
+			for(int col = 0; col < GameMain.COLS; ++col) {
+				if(cells[row][col].content == Player.Empty) {
+					return false; // Empty cell occurs, so not a draw
+				}
+			}
+		}
 		
-
-		
+		return true; // If no empty cells found, it's a draw
+			
 	}
 	
 	/** Return true if the current player "thePlayer" has won after making their move  */
